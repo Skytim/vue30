25c8d9f712326debf4accbc958cb2a5f81b932dd
Tim Tsai <tim.tsai@xuenn.com>
2021-02-24 18:24:29 +0800

    Flex Panel Gallery

--- 11 - Custom Video Player/scripts-FINISHED copy.js
+++ 11 - Custom Video Player/scripts-FINISHED copy.js
@@ -1,28 +1,28 @@
 /* Get Our Elements */
-const player = document.querySelector(".player");
-const video = player.querySelector(".viewer");
-const progress = player.querySelector(".progress");
-const progressBar = player.querySelector(".progress__filled");
-const toggle = player.querySelector(".toggle");
-const skipButtons = player.querySelectorAll("[data-skip]");
-const ranges = player.querySelectorAll(".player__slider");
+// const player = document.querySelector(".player");
+// const video = player.querySelector(".viewer");
+// const progress = player.querySelector(".progress");
+// const progressBar = player.querySelector(".progress__filled");
+// const toggle = player.querySelector(".toggle");
+// const skipButtons = player.querySelectorAll("[data-skip]");
+// const ranges = player.querySelectorAll(".player__slider");
 
 /* Hook up the event listeners */
-video.addEventListener("click", togglePlay);
-video.addEventListener("play", updateButton);
-video.addEventListener("pause", updateButton);
-video.addEventListener("timeupdate", handleProgress);
+// video.addEventListener("click", togglePlay);
+// video.addEventListener("play", updateButton);
+// video.addEventListener("pause", updateButton);
+// video.addEventListener("timeupdate", handleProgress);
 
-toggle.addEventListener("click", togglePlay);
-skipButtons.forEach((button) => button.addEventListener("click", skip));
-ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
-ranges.forEach((range) => range.addEventListener("mousemove", handleRangeUpdate));
+// toggle.addEventListener("click", togglePlay);
+// skipButtons.forEach((button) => button.addEventListener("click", skip));
+// ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+// ranges.forEach((range) => range.addEventListener("mousemove", handleRangeUpdate));
 
-let mousedown = false;
-progress.addEventListener("click", scrub);
-progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
-progress.addEventListener("mousedown", () => (mousedown = true));
-progress.addEventListener("mouseup", () => (mousedown = false));
+// let mousedown = false;
+// progress.addEventListener("click", scrub);
+// progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+// progress.addEventListener("mousedown", () => (mousedown = true));
+// progress.addEventListener("mouseup", () => (mousedown = false));
 
 const app = {
     methods: {
@@ -60,6 +60,7 @@ const app = {
         setInterval(() => {
             this.setDate();
         }, 1000);
+        console.log()
     },
 };
 Vue.createApp(app).mount("#app");
