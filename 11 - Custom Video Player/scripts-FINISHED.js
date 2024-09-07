5199f53396718c12603569bd9473cced4393cccb
SkyTim <sky22357168@gmail.com>
2024-09-07 18:42:55 +0800

    feat: Custom Video Player  #11

--- 11 - Custom Video Player/scripts-FINISHED.js
+++ 11 - Custom Video Player/scripts-FINISHED.js
@@ -1,38 +1,60 @@
+const { ref, onMounted } = Vue;
+
 const app = {
-    data: function () {
+    setup() {
+        const mousedown = ref(false);
+        const video = ref(null);
+        const toggle = ref(null);
+        const progress = ref(null);
+        const progressBar = ref(null);
+
+        const togglePlay = () => {
+            const method = video.value.paused ? "play" : "pause";
+            video.value[method]();
+            updateButton();
+        };
+
+        const updateButton = () => {
+            const icon = video.value.paused ? "►" : "❚ ❚";
+            toggle.value.textContent = icon;
+        };
+
+        const skip = (e) => {
+            video.value.currentTime += parseFloat(e.target.dataset.skip);
+        };
+
+        const handleRangeUpdate = (e) => {
+            video.value[e.target.name] = e.target.value;
+        };
+
+        const handleProgress = () => {
+            const percent = (video.value.currentTime / video.value.duration) * 100;
+            progressBar.value.style.flexBasis = `${percent}%`;
+        };
+
+        const scrub = (e) => {
+            const scrubTime = (e.offsetX / progress.value.offsetWidth) * video.value.duration;
+            video.value.currentTime = scrubTime;
+        };
+
+        onMounted(() => {
+            video.value.addEventListener("click", togglePlay);
+        });
+
         return {
-            mousedown: false,
+            mousedown,
+            video,
+            toggle,
+            progress,
+            progressBar,
+            togglePlay,
+            updateButton,
+            skip,
+            handleRangeUpdate,
+            handleProgress,
+            scrub,
         };
     },
-    methods: {
-        togglePlay() {
-            const method = this.$refs.video.paused ? "play" : "pause";
-            this.$refs.video[method]();
-        },
-
-        updateButton(e) {
-            const icon =  this.$refs.video.paused ? "►" : "❚ ❚";
-            console.log(icon);
-            this.$refs.toggle.textContent = icon;
-        },
-
-        skip(e) {
-            this.$refs.video.currentTime += parseFloat(e.target.dataset.skip);
-        },
-
-        handleRangeUpdate(e) {
-            this.$refs.video[e.target.name] = e.target.value;
-        },
-
-        handleProgress() {
-            const percent = (this.$refs.video.currentTime / this.$refs.video.duration) * 100;
-            this.$refs.progressBar.style.flexBasis = `${percent}%`;
-        },
-        scrub(e) {
-            const scrubTime = (e.offsetX / this.$refs.progress.offsetWidth) * this.$refs.video.duration;
-            this.$refs.video.currentTime = scrubTime;
-        },
-    },
-    mounted() {},
 };
+
 Vue.createApp(app).mount("#app");
